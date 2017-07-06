20646d6113628d73a0eab4cc7d51b8bcd6c31a47
nguyenductrong <nguyenductrong281293@gmail.com>
2017-07-06 17:59:55 +0700

    update index.html, styles.css, monster.js

--- gameMonster/js/monster.js
+++ gameMonster/js/monster.js
@@ -2,7 +2,6 @@ var container = document.getElementById("container");
 var header = document.getElementById("header");
 var context = container.getContext('2d');
 var contextAction = header.getContext('2d');
-
 document.body.appendChild(container);
 var score = 10;
 var tempScore = 10;
@@ -16,66 +15,60 @@ var booleanBoom = true;
 var booleanStop = false;
 var booleanPause = false;
 var run = true;
-var backgroundContainer = new Image();
-var listBoold = new Array();
+var listBlood = new Array();
 
 // setBackground container
+var backgroundContainer = new Image();
 backgroundContainer.onload = function() {
 	setBackground = true;
 	
-};
+}
 backgroundContainer.src = ("images/background.png");
 
+// Check browser support
+if(typeof(Storage) != "underfined") {
+	localStorage.setItem("tempScore", tempScore); // add tempScore in list Storage.
+}
 
 var imageMonter = new Image();
 imageMonter.onload = function() {
-		
-};
+}
 imageMonter.src = "images/monster.png";
 
 // create btnBoom
-
 var btnBoom = new Image();
-btnBoom.onload = function() {
-	
-};
+btnBoom.onload = function() {	
+}
 btnBoom.src = "images/boom.png"
 
 //creat btnStop
 var btnStop = new Image();
-btnStop.onload = function() {
-	
-};
+btnStop.onload = function() {	
+}
 btnStop.src = "images/stop.png";
 
 // create btnPause
 var btnPause = new Image();
-btnPause.onload = function() {
-	
-};
+btnPause.onload = function() {	
+}
 btnPause.src = "images/pause.png";
 
 // create btnRestart
 var btnRestart = new Image();
-btnRestart.onload = function() {
-	
-};
+btnRestart.onload = function() {	
+}
 btnRestart.src = "images/restart.png";
 
 // create heartImg
 var itemHeart = new Image();
-itemHeart.onload = function() {
-	
-};
-
+itemHeart.onload = function() {	
+}
 itemHeart.src = "images/heart.png"
 
 // create blood
 var blood = new Image();
-
-blood.onload = function() {
-	
-};
+blood.onload = function() {	
+}
 blood.src = "images/blood.png";
 
 // New class Monster
@@ -109,38 +102,434 @@ var monster5 = new Monster( 380, 380, 260, 260, 380, 380, 260, 260, speedOfPlay,
 var monster6 = new Monster(190, 380, 190, 260, 190, 380, 190, 260, speedOfPlay, false, false, 0,0);
 
 
-var monster7 = new Monster(0, 380, 120, 260, 0, 380, 120, 260, speed, false, false, 0, 0);
+var monster7 = new Monster(0, 380, 120, 260, 0, 380, 120, 260, speedOfPlay, false, false, 0, 0);
 
-var monster8 = new Monster(0, 190, 120, 190, 0, 190, 120, 190, speed, false, false, 0, 0);
+var monster8 = new Monster(0, 190, 120, 190, 0, 190, 120, 190, speedOfPlay, false, false, 0, 0);
 
-var Monster9 = {
-	startX: Math.floor((Math.random() * 500) + 1),
-	startY: Math.floor((Math.random() * 500) + 1),
-	stopX: Math.floor((Math.random() * 500) + 1),
-	stopY: Math.floor((Math.random() * 500) + 1),
-	speed: 1,
-	click: false,
-	show: true,
-	dieX: 0,
-	dieY:0
+var monster9 = (Math.floor((Math.random() * 500) + 1),
+				Math.floor((Math.random() * 500) + 1),
+				Math.floor((Math.random() * 500) + 1),
+				Math.floor((Math.random() * 500) + 1),
+				Math.floor((Math.random() * 500) + 1),
+				Math.floor((Math.random() * 500) + 1),
+				Math.floor((Math.random() * 500) + 1),
+				Math.floor((Math.random() * 500) + 1),
+				1, false, true, 0, 0);
+
+// creat event click for buttons
+header.addEventListener("click", function(e) {
+	var locationX = e.pageX - this.offsetLeft;
+	var locationY = e.pageY - this.offsetTop;
+	
+	// check click boom
+	if (locationX > 290 && locationX < 340 && locationY > 60 && locationY < 100) {
+		if (booleanBoom) {
+			console.log("BOOM");
+			clickBoom();
+			numberBoom --;
+			if (numberBoom <= 0) {
+				booleanBoom = false;
+			}
+		}
+	}	
+	// check click stop
+	if (locationX > 350 && locationX < 390 && locationY > 60 && locationY < 100 ) {
+		console.log("STOP");
+		if (numberStop > 0) {
+			if (run) {
+				run = false ;
+				booleanStop = true;
+				numberStop --;
+			} else {
+				run = true;
+				main();
+				booleanStop = false;
+			}
+			setTimeout(function() {
+				run = true;
+				main();
+				booleanStop = true;
+			},3000);
+		} else {
+			run = true;
+			main();
+			booleanStop = false;
+		}
+	}
+	//check click pause
+	if (locationX > 400 && locationX< 440 && locationY > 60 && locationY < 100) {
+		console.log("PAUSE");
+		if (run){
+			run = false;
+			booleanPause = true;
+		}else {
+			run = true;
+			main();
+			booleanPause = false;
+		}	
+	}
+	
+	// check click restart
+	if (locationX > 450 && locationX < 490 && locationY > 60 && locationY < 100) {
+		console.log("RESTART");
+		context.clearRect(0, 0, container.width, container.height);
+		restart();
+		main();
+	}
+})
+
+// even click for Monster in the area container
+container.addEventListener("click", function (e){
+	locationX = e.pageX - this.offsetLeft; // position mouse 
+	locationY = e.pageY - this.offsetTop;
+	console.log(locationX);
+	console.log(locationY);
+	if (!booleanPause || booleanStop) {
+		if (monster1.show) {
+			executeActionMonsterDie(monster1, locationX, locationY);
+			
+		} 
+		if (monster2.show) {
+			executeActionMonsterDie(monster2, locationX, locationY);
+			
+		}
+		if (monster3.show) {
+			executeActionMonsterDie(monster3, locationX,locationY);
+			
+		}
+		if (monster4.show) {
+			executeActionMonsterDie(monster4, locationX,locationY);
+			
+		}
+		if (monster5.show) {
+			executeActionMonsterDie(monster5, locationX,locationY);
+			
+		}
+		if (monster6.show) {
+			executeActionMonsterDie(monster6, locationX,locationY);
+			
+		}
+		if (monster7.show) {
+			executeActionMonsterDie(monster7, locationX,locationY);
+			
+		}
+		if (monster8.show) {
+			executeActionMonsterDie(monster8, locationX,locationY);
+			
+		}
+		if (monster9.show) {
+			executeActionMonsterDie(monster9, locationX,locationY);	
+		}	
+	}
+	
+},false);
+
+/*
+  funciton click on the monster
+  @param locationX, locationY : location mouster
+*/
+var executeActionMonsterDie = function(monster, locationX, locationY) {
+	var countClickMonster = 0;
+	var countClick = 0;
+	if (monster.click) {
+		if (monster.startX < locationX && locationX < (monster.startX + imageMonter.width) && locationY < (monster.startY + imageMonter.height)) {
+		var soundClick = new Audio('audio/click.wav');
+		soundClick.play();
+		score += 10;
+		tempScore += 10;
+		countClickMonster +=1;
+		monster.click = false;
+		monster.show = false;
+		monster.dieX = locationX;
+		monster.dieY = locationY;
+		monster.startX = monster.beginX;
+		monster.startY = monster.beginY;
+		monster.stopX = monster.endX;
+		monster.stopY = monster.endY;
+        console.log("Number click monster: " + countClickMonster);
+		if(countClickMonster == 20){
+		  heart ++;
+		  countClickMonster = 0;
+		}
+		if(countClickMonster % 5 == 0){
+			monster9.show = true;
+			console.log("show monster9");
+		}
+		if(countClickMonster == 50){
+			numberBoom += 1;
+		}
+		for (var i = 0; i < numberMonster ; i ++) {
+			randomMonster();	
+		}
+		addPositionMonsterDie(monster.dieX, monster.dieY);
+		drawContainer();
+		
+		}else {
+			score -= 5;
+		    tempScore -= 5;
+			countClick ++;
+			if (countClick == 5) {
+				heart --;
+			}
+		}	
+	} 	
 }
 
-var updateBlood = function(){
+/*
+ function save position monster die
+ @param: positionX: location monsterDieX
+         positonY: location monsterDieY
+*/
+var addPositionMonsterDie = function(positionX, positionY) {
+	var itemBoold = { 
+		positionX: positionX,
+		positionY: positionY
+	}
+	listBlood[listBlood.length] = itemBoold;	
+}
+
+// function create a random monster
+var randomMonster = function () {
+	var value = Math.floor( (Math.random() * 8) + 1);
+	console.log("value random: " + value);
+	switch (value) {
+		case 1:
+		if (!monster1.show) {
+			monster1.show = true;
+		}
+		break;
+		case 2:
+		if (!monster2.show) {
+			monster2.show = true;
+		}
+		break;
+		case 3:
+		if (!monster3.show) {
+			monster3.show = true;
+		}
+		break;
+		case 4:
+		if (!monster4.show) {
+			monster4.show = true;
+		}
+		break;
+		case 5:
+		if (!monster5.show) {
+			monster5.show = true;
+		}
+		break;
+		case 6:
+		if (!monster6.show) {
+			monster6.show = true;
+		}
+		break;
+		case 7:
+		if (!monster7.show) {
+			monster7.show = true;
+		}
+		break;
+		case 8:
+		if (!monster8.show) {
+			monster8.show = true;
+		}
+		break;	
+	}	
+}
+
+// function speed by score
+var leverPlay = function() {
+	var lever = tempScore/100;
+	console.log("Lever: " + parseInt(lever));
+	switch(parseInt(lever)) {
+		case 1:
+		speedOfPlay = 1;
+		numberMonster = 1;
+		break;
+		case 2:
+		speedOfPlay = 2;
+		numberMonster = 2;
+		break;
+		case 4:
+		speedOfPlay = 4;
+		numberMonster = 4;
+		break;
+		case 6:
+		speedOfPlay = 6;
+		numberMonster = 6;
+		break;
+	}
+}
+
+// function click boom
+var clickBoom = function() {
+	if (monster1.show) {
+		score += 10;
+		tempScore += 10;
+		monster1.show = true;
+		monster1.click = false;
+		addPositionMonsterDie(monster1.startX, monster1.startY);
+	}
+	if (monster2.show) {
+		score += 10;
+		tempScore += 10;
+		monster2.show = true;
+		monster2.click = false;
+		addPositionMonsterDie(monster2.startX, monster2.startY);
+	}
+	if (monster3.show) {
+		score += 10;
+		tempScore += 10;
+		monster3.show = true;
+		monster3.click = false;
+		addPositionMonsterDie(monster3.startX, monster3.startY);
+	}
+	if (monster4.show) {
+		score += 10;
+		tempScore += 10;
+		monster4.show = true;
+		monster4.click = false;
+		addPositionMonsterDie(monster4.startX, monster4.startY);
+	}
+	if (monster5.show) {
+		score += 10;
+		tempScore += 10;
+		monster5.show = true;
+		monster5.click = false;
+		addPositionMonsterDie(monster5.startX, monster5.startY);
+	}
+	if (monster6.show) {
+		score += 10;
+		tempScore += 10;
+		monster6.show = true;
+		monster6.click = false;
+		addPositionMonsterDie(monster6.startX, monster6.startY);
+	}
+	if (monster7.show) {
+		score += 10;
+		tempScore += 10;
+		monster7.show = true;
+		monster7.click = false;
+		addPositionMonsterDie(monster7.startX, monster7.startY);
+	}
+	if (monster8.show) {
+		score += 10;
+		tempScore += 10;
+		monster8.show = true;
+		monster8.click = false;
+		addPositionMonsterDie(monster8.startX, monster8.startY);
+	}
+	if (monster9.show) {
+		score += 10;
+		tempScore += 10;
+		monster9.show = true;
+		monster9.click = false;
+		addPositionMonsterDie(monster9.startX, monster9.startY);
+	}
+	speedOfPlay = speedOfPlay; 
+	var soundBoom = new Audio('audio/boom.mp3');
+	soundBoom.play();
+	drawContainer();
+	for (var i = 0; i < numberBoom; i ++){
+		randomMonster();
+	}
+}
+
+/*
+ funcition refresh monster
+ @param monster
+
+*/
+var refreshMonster = function (monster) {
+	monster.show = false;
+	monster.click = false;
+	monster.startX = monster.beginX;
+	monster.startY = monster.beginY
+	monster.stopX = monster.endX;
+	monster.stopY = monster.endY;
+}
+
+// function click restart
+var restart = function() {
+	speed = 1;
+	run = true;
+	score = 10;
+	tempScore = 10;
+	heart = 5;
+	numberBoom = 3;
+	numberStop = 3;
+	numberMonster = 1;
+	setBackground = true;
+	booleanBoom = true;
+	booleanStop = false;
+	booleanPause = false;
+	listBlood = new Array();
+	run = true;
+	refreshMonster(monster1);
+	refreshMonster(monster2);
+	refreshMonster(monster3);
+	refreshMonster(monster4);
+	refreshMonster(monster5);
+	refreshMonster(monster6);
+	refreshMonster(monster7);
+	refreshMonster(monster8);
+	refreshMonster(monster9);
+	monster1.show = true;    // after refresh monster1 appear first
+}
+// function update location move monster
+var updateMoveMonster = function(monster) {
+	monster.click = true;
 	
-};
+	if (monster.startX > monster.stopX) {
+		monster.startX -= monster.speed;
+	} else if(monster.startX < monster.stopX) {
+		monster.startX += monster.speed;
+	}
+	
+	if(monster.startY > monster.stopY) {
+		monster.startY -= monster.speed;
+	} else if(monster.startY < monster.stopY) {
+		monster.startY += monster.speed;
+	}
+	
+	if(monster.startX == monster.stopX && monster.startY == monster.stopY) {
+		monster.startX = monster.stopX;
+		monster.startY = monster.startY;
+		monster.stopX = monster.beginX;
+		monster.stopY = monster.beginY;
+	}
+	
+	if(monster.startX == monster.beginX && monster.startY == monster.beginY) {
+		monster.show = false;
+		monster.startX = monster.beginX;
+		monster.startY = monster.beginY;
+		monster.stopX = monster.endX;
+		monster.stopY = monster.endY;
+		score -= 10;
+	}
+	
+}
+
+// draw blood at monster die blood(positionX, positionY)
+var updateBlood = function() {
+	if (listBlood.length > 0) {
+		for (var i = 0; i < listBlood.length; i ++) {
+			context.drawImage(blood,listBlood[i].positionX, listBlood[i].positionY);
+		}
+	}		
+}
 
 // create Image btnPause,btnRestart, btnBoom .......... of header;
-var createHeader = function(){
+var createHeader = function() {
 	contextAction.clearRect(0,0,header.width,header.height);
-	contextAction.fillStyle("#007f7f");
+	contextAction.fillStyle = "#007f7f";
 	contextAction.font = "20px Arial";
 	contextAction.fillText("Score: " + score, 10 , 30);
 	contextAction.fillText("Random Monster: " + numberMonster,300, 30);
 	contextAction.fillText("Heart: ", 10,60);
-	contextAction.fillText("Speed: " + speed, 10, 90);
+	contextAction.fillText("Speed: " + speedOfPlay, 10, 90);
 	var item = 0;	
-	for(var i = 0; i < heart; i ++){
-		contextAction.drawImage(heartImg,(item + 70),45,20,20);
+	for(var i = 0; i < heart; i ++) {
+		contextAction.drawImage(itemHeart,(item + 70),45,20,20);
 		item += 20;
 	}
 	contextAction.drawImage(btnBoom, 290, 60, 50, 40);
@@ -152,13 +541,17 @@ var createHeader = function(){
 	contextAction.fillText(numberBoom,300,75);
 	contextAction.fillText(numberStop,360,75);
 	contextAction.font = "20px Arial";
-};
+}
+
 
 // function load and draw pictures
-var drawContainer = function(){
+var drawContainer = function() {
+	
 	if (setBackground) { 
 		context.drawImage(backgroundContainer,0,0,container.width,container.height);
 	}
+	
+	updateBlood();
 	
 	if(monster1.show) {
 		context.drawImage(imageMonter, monster1.startX, monster1.startY);
@@ -199,195 +592,77 @@ var drawContainer = function(){
 		context.font = "50px Arial";
 		context.fillText("Pause!!!", 180, 240);
 	}
-};
+}
 
-// creat event click for buttons
-header.addEventListener("click", function(e) {
-	var locationX = e.pageX - this.offsetLeft;
-	var locationY = e.pageX - this.offTop;
-	
-	//button pause
-	if (locationX > 400 && locationX< 440 && locationY > 60 && locationY < 100) {
-		console.log("PAUSE");
-		if (run){
-			run = false;
-			booleanPause = true;
-		}else {
-			run = true;
-			
-			booleanPause = false;
-		}
-		
-		
-	}
-	
-	// button boom
-	if (locationX > 290 && locationX < 340 && locationY > 60 && locationY < 100) {
-		if (booleanBoom) {
-			console.log("BOOM");
-			numberBoom --;
-			if (numberBoom <= 0) {
-				booleanBoom = false;
-			}
-		}
-	}
-	
-	// button stop
-	
-	if (locationX > 350 && locationX < 390 && locationY > 60 && locationY < 100 ) {
-		console.log("STOP");
-		booleanPause = false;
-		if (numberStop > 0) {
-			if (run) {
-				run = false ;
-				booleanStop = true;
-				numberStop --;
-				
-			} else {
-				run = true;
-				booleanStop = false;
-			}
-		} else {
-			run = true;
-			booleanStop = false;
-		}
-	}
-	
-	// button restart 
-	if (locationX > 450 && locationX < 490 && locationY > 60 && locationY < 100) {
-		console.log("RESTART");
-		restart() ;
-	}
-});
+// function show gameOver
+var gameOver = function() {
+	var soundGameOver = new Audio("audio/gameOver.mp3");
+	soundGameOver.play();
+	context.fillStyle = "#FFFFFF";
+	context.font = "40px Arial";
+	context.fillText("Game Over!!!", 130, 200);
+	context.font = "20px Arial";
+	context.fillStyle = "#5bfa3f";
+	context.fillText("Score = " + score, 130, 240);
+	context.fillText("Best score = " + localStorage.getItem("tempScore"), 130, 280);	
+}
 
-// even click for Monster in the area container
-container.addEventListener("click", function (e){
-	if (booleanPause) {
-		score -= 5;
-		tempScore -= 5;		
-	}
-	if(booleanStop){
-		
-	}
+// funcition run 
+var main = function() {
+	leverPlay();
 	
+	if(monster1.show) {
+		updateMoveMonster(monster1);
+	}
+	if(monster2.show) {
+		updateMoveMonster(monster2);
+	}
+	if(monster3.show) {
+		updateMoveMonster(monster3);
+	}
+	if(monster4.show) {
+		updateMoveMonster(monster4);
+	}
+	if(monster5.show) {
+		updateMoveMonster(monster5);
+	}
+	if(monster6.show) {
+		updateMoveMonster(monster6);
+	}
+	if(monster7.show) {
+		updateMoveMonster(monster7);
+	}
+	if(monster8.show) {
+		updateMoveMonster(monster8);
+	}
+	if(monster9.show) {
+		updateMoveMonster(monster9);
+	}
+	drawContainer();
 	
-},false);
-
-var restart = function() {
-	speed = 1;
-	run = true;
-	score = 10;
-	tempScore = 10;
-	heart = 5;
-	numberBoom = 3;
-	numberStop = 3;
-	numberMonster = 1;
-	setBackground = true;
-	booleanBoom = true;
-	booleanStop = false;
-	booleanPause = false;
-	run = true;
+	if(score <= 0) {
+		gameOver();
+	} else if(heart == 0) {
+		var bestScore = parseInt(localStorage.getItem("tempScore"));
+		if(tempScore < score) {
+			localStorage.setItem("tempScore", score);			
+		}
+		gameOver();
+	}else {
+		if(run) {
+			requestAnimationFrame(main);
+		}
+	}
 	
 }
 
-var executeActionMonsterDie = function(monster, locationX, locationY) {
-	var countClick = 0;
-	if (monster.click) {
-		if (monster.startX < locationX && locationX < (monster.startX + monsterImage.width) && monster.startY < locationY && locationY < (monster.startY + monsterImage.height)) {
-		var soundClick = new Audio('music/audio.mp3');
-		soundClick.play();
-		score += 10;
-		tempScore += 10;
-		countClick ++;
-		monster.click = false;
-		monster.show = false;
-		monster.dieX = monster.startX;
-		monster.diey = monster.startY;
-		monster.startX = monster.beginX;
-		monster.startY = monster.beginY;
-		monster.stopX = monster.endX;
-		monster.stopY = monster.endY;
-		addPositionMonsterDie (monster.dieX, monster.dieY);
-		
-		if(countClick == 30){
-		  heart ++;
-		  countClick = 0;
-		}
-		if(countClick % 5 == 0){
-			Monster9.show = true;
-			console.log("show monster9");
-		}
-		random();
-		
-		}
-	} else {
-		score -= 5;
-		tempScore -= 5;	
-	}
-	
-	
-}
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame
+						|| w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+main();	
 
-var executeActionMonster = function (monster, locationX, locationY) {
-	
-}
 
-var randomMonster = function () {
-	var value = Math.floor( Math.random() * 8);
-	switch (value) {
-		case 1:
-		if (!Monster1.show){
-			Monster1.show = true;
-		}
-		break;
-		case 2:
-		if (!Monster2.show){
-			Monster2.show = true;
-		}
-		break;
-		case 3:
-		if (!Monster3.show){
-			Monster3.show = true;
-		}
-		break;
-		case 4:
-		if (!Monster4.show){
-			Monster4.show = true;
-		}
-		break;
-		case 5:
-		if (!Monster5.show){
-			Monster5.show = true;
-		}
-		break;
-		case 6:
-		if (!Monster6.show){
-			Monster6.show = true;
-		}
-		break;
-		case 7:
-		if (!Monster7.show){
-			Monster7.show = true;
-		}
-		break;
-		case 8:
-		if (!Monster8.show){
-			Monster8.show = true;
-		}
-		break;
-		
-	}
-	
-	
-}
 
-var addPositonMonsterDie = function (positonX, positionY){
-var imteBoold = { 
-	positionX : positionX,
-	positionY : positionY
-	};
-	listBoold.unshift(itemBlood);	
-};
 
 
 
